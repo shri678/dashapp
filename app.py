2048c6ce9d121b78a5c26c25290b07e597d2f566
dirkkoolmees <65215053+dirkkoolmees@users.noreply.github.com>
2020-07-24 13:19:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import plotly.express as px
-from jupyter_dash import JupyterDash
+#from jupyter_dash import JupyterDash
+import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
@@ -12,7 +13,7 @@ url = 'https://raw.githubusercontent.com/dirkkoolmees/CO2_emissions_per-region/m
 df = pd.read_csv(url, index_col = 'Year')
 
 # Build App
-app = JupyterDash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div([
